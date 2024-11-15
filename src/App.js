958490c4049275d24c941bbef787d70264690702
jamdazai <121931347+jamdazai@users.noreply.github.com>
2024-11-14 19:40:22 -0500

    Pushing codes for Lab Exercise 9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import UserList from './components/UserList';
+import { Routes, Route, BrowserRouter, NavLink, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import AboutUs from './components/AboutUs';
+import UserDetails from './components/UserDetails';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  render() {
+    return (
+    <div>
+      <h1>Axios and Navigation</h1>
+      {/* <UserList /> */}
+      <BrowserRouter>
+      <nav>
+        <Link to='/home'>Home</Link> | 
+        <Link to='/about?college=GBC'> About</Link> | 
+        <NavLink to='/contact/Jam Furaque'> Contact</NavLink> | 
+        <NavLink to='/userlist'> User List</NavLink>
+      </nav>
+        <Routes>
+          <Route path='/home' element= { <Home/> }/>
+          <Route path='/about' element= { <AboutUs/> }/>
+          <Route path='/contact/:name' element= { <Contact/> }/>
+          <Route path='/userlist' element= { <UserList /> } />
+          <Route path='/user/:userid' element= { <UserDetails /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )}
 }
-
-export default App;
